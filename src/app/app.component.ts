f2cbc5ecbcfd7edf3aa4ec7a81e99b20f279d8f9
aniket2727 <aniketkadam5950@gmail.com>
2024-10-14 12:37:59 +0530

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';  // Import RouterModule for routing
+import { FooterComponent } from './footer/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
